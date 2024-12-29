16c710061dd346bb03e96a49398636e8017a5ee5
Shlok <shlokodre@gmail.com>
2024-12-29 16:31:01 +0530

    hi

--- vedant.java
+++ vedant.java
@@ -1,5 +1,5 @@
 public class vedant{
-    public static void main(string[]args){
+    public static void main(String[]args){
         System.out.println("hello moglas");
         System.out.println("hello darshan ");
     }
